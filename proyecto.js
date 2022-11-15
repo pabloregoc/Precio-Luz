2624c73ae9c90cd170dcd2cfceeddfb2209c1dd6
pabloregoc <114402687+pabloregoc@users.noreply.github.com>
2022-11-15 20:22:15 +0100

    Add files via upload

--- proyecto.js
+++ proyecto.js
@@ -12,13 +12,9 @@ async function api() {
     arrayElectricidad.push(ObjetoCentral[i].price);
   }
   const arrayPorHoras = [].concat(arrayElectricidad);
-  console.log(arrayElectricidad.sort());
-  console.log(arrayPorHoras);
 
   let precioMaximo = arrayElectricidad.sort()[23] / 1000;
-  console.log(precioMaximo);
   let precioMinimo = arrayElectricidad.sort()[0] / 1000;
-  console.log(precioMinimo);
   let precioActual = "";
   let datoHora = new Date();
   let horaActual = datoHora.getHours();
@@ -99,25 +95,97 @@ async function api() {
   }
   precioActual = x / 1000;
 
-  console.log(x);
-
   const cabeceraMin = document.querySelector(`.precioMinimo`);
   const parrafo1 = document.createElement("p");
-  const textoParrafo1 = document.createTextNode(precioMinimo);
+  const textoParrafo1 = document.createTextNode(`${precioMinimo} Kw/H `);
   parrafo1.appendChild(textoParrafo1);
   cabeceraMin.appendChild(parrafo1);
 
   const cabeceraMax = document.querySelector(`.precioMaximo`);
   const parrafo2 = document.createElement("p");
-  const textoParrafo2 = document.createTextNode(precioMaximo);
+  const textoParrafo2 = document.createTextNode(`${precioMaximo} Kw/H`);
   parrafo2.appendChild(textoParrafo2);
   cabeceraMax.appendChild(parrafo2);
 
   const cabeceraAct = document.querySelector(`.precioActual`);
   const parrafo3 = document.createElement("p");
-  const textoParrafo3 = document.createTextNode(precioActual);
+  const textoParrafo3 = document.createTextNode(`${precioActual} Kw/H`);
   parrafo3.appendChild(textoParrafo3);
   cabeceraAct.appendChild(parrafo3);
+
+  const horaNevera = [precioActual * 180] / 8760;
+  const horaLavavajillas = precioActual * 0.246;
+  const horaLavadora = precioActual * 0.579;
+  const horaSecadora = precioActual * 0.27;
+  const horaHorno = precioActual;
+  const horaVitro = precioActual * 0.825;
+  const horaBombilla = precioActual * 0.1;
+  const horaOrdenador = precioActual * 2.2;
+  console.log(horaOrdenador);
+
+  const divNevera = document.querySelector(`.nevera`);
+  const parrafo4 = document.createElement("p");
+  const textoParrafo4 = document.createTextNode(
+    `Mantener encendida tu nevera te cuesta ${horaNevera} céntimos por hora`
+  );
+  parrafo4.appendChild(textoParrafo4);
+  divNevera.appendChild(parrafo4);
+
+  const divLavavajillas = document.querySelector(`.lavavajillas`);
+  const parrafo5 = document.createElement("p");
+  const textoParrafo5 = document.createTextNode(
+    `Mantener encendido tu lavavajillas te cuesta ${horaLavavajillas} céntimos por hora`
+  );
+  parrafo5.appendChild(textoParrafo5);
+  divLavavajillas.appendChild(parrafo5);
+
+  const divLavadora = document.querySelector(`.lavadora`);
+  const parrafo6 = document.createElement("p");
+  const textoParrafo6 = document.createTextNode(
+    `Mantener encendida tu lavadora te cuesta ${horaLavadora} céntimos por hora`
+  );
+  parrafo6.appendChild(textoParrafo6);
+  divLavadora.appendChild(parrafo6);
+
+  const divSecadora = document.querySelector(`.secadora`);
+  const parrafo7 = document.createElement("p");
+  const textoParrafo7 = document.createTextNode(
+    `Mantener encendida tu secadora te cuesta ${horaSecadora} céntimos por hora`
+  );
+  parrafo7.appendChild(textoParrafo7);
+  divSecadora.appendChild(parrafo7);
+
+  const divHorno = document.querySelector(`.horno`);
+  const parrafo8 = document.createElement("p");
+  const textoParrafo8 = document.createTextNode(
+    `Mantener encendida tu horno te cuesta ${horaHorno} céntimos por hora`
+  );
+  parrafo8.appendChild(textoParrafo8);
+  divHorno.appendChild(parrafo8);
+
+  const divVitro = document.querySelector(`.vitro`);
+  const parrafo9 = document.createElement("p");
+  const textoParrafo9 = document.createTextNode(
+    `Mantener encendida tu vitrocerámica te cuesta ${horaVitro} céntimos por hora`
+  );
+  parrafo9.appendChild(textoParrafo9);
+  divVitro.appendChild(parrafo9);
+
+  const divBombilla = document.querySelector(`.bombilla`);
+  const parrafo10 = document.createElement("p");
+  const textoParrafo10 = document.createTextNode(
+    `Mantener encendida cada bombilla te cuesta ${horaBombilla} céntimos por hora`
+  );
+  parrafo10.appendChild(textoParrafo10);
+  divBombilla.appendChild(parrafo10);
+
+  const divOrdenador = document.querySelector(`.ordenador`);
+  const parrafo11 = document.createElement("p");
+  const textoParrafo11 = document.createTextNode(
+    `Mantener encendido tu ordenador te cuesta ${horaOrdenador} céntimos por hora`
+  );
+  parrafo11.appendChild(textoParrafo11);
+  divOrdenador.appendChild(parrafo11);
 }
 
 api();
@@ -142,3 +210,10 @@ const intervaloHora = setInterval(() => {
 
 // console.log(respuesta);
 //console.log(datos);
+
+//console.log(x);
+
+//console.log(precioMaximo);
+//console.log(precioMinimo);
+//console.log(arrayElectricidad.sort());
+// console.log(arrayPorHoras);
